8d9c9c842bce4e02d05016c74511f332ebe800c1
brauliorivas <61257604+brauliorivas@users.noreply.github.com>
2024-07-17 15:34:23 +0000

    Deploying to gh-pages from @ brauliorivas/dmx@2659b8f4b4d9ba017a5db763c1dc678477b283b6 🚀

--- main/js/views/views.js
+++ main/js/views/views.js
@@ -1,5 +1,6 @@
 import { currentObjects, currentEvent } from "../event-number.js";
 import { copyObject } from "../lib/copy.js";
+import { checkEmptyObject } from "../lib/empty-object.js";
 import { getVisible } from "../events.js";
 import { drawAll } from "../draw.js";
 import { canvas } from "../main.js";
@@ -11,6 +12,9 @@ import {
   mouseMove,
   onScroll,
 } from "../events.js";
+import { emptyViewMessage, hideEmptyViewMessage } from "../lib/messages.js";
+import { showViewInformation, hideViewInformation } from "../information.js";
+import { emptyCanvas } from "../draw.js";
 
 const currentView = {};
 
@@ -37,6 +41,11 @@ function scroll() {
   window.scrollTo(scrollLocations[index].x, scrollLocations[index].y);
 }
 
+function setInfoButtonName(view) {
+  const button = document.getElementById("view-information-button");
+  button.innerText = view;
+}
+
 const drawView = (view) => {
   paintButton(view);
 
@@ -47,14 +56,29 @@ const drawView = (view) => {
     prevMouseY: 0,
   };
 
-  const { preFilterFunction, viewFunction, scrollFunction, filters } =
-    views[view];
+  const {
+    preFilterFunction,
+    viewFunction,
+    scrollFunction,
+    filters,
+    description,
+  } = views[view];
 
   const viewObjects = {};
   const viewCurrentObjects = {};
   const viewVisibleObjects = {};
 
   preFilterFunction(currentObjects, viewObjects);
+  const isEmpty = checkEmptyObject(viewObjects);
+
+  if (isEmpty) {
+    emptyCanvas();
+    emptyViewMessage();
+    hideViewInformation();
+    return;
+  }
+  showViewInformation(view, description);
+  hideEmptyViewMessage();
   viewFunction(viewObjects);
   copyObject(viewObjects, viewCurrentObjects);
 
@@ -69,6 +93,7 @@ const drawView = (view) => {
   drawAll(viewCurrentObjects);
   getVisible(viewCurrentObjects, viewVisibleObjects);
   filters(viewObjects, viewCurrentObjects, viewVisibleObjects);
+  setInfoButtonName(getView());
 
   canvas.onmousedown = (event) => {
     mouseDown(event, viewVisibleObjects, dragTools);
